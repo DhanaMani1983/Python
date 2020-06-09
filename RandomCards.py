9264ae0263770d721e7e84b8080ae669033df9d7
DhanaMani1983 <Dhana.Mani@hotmail.co.uk>
2020-06-10 00:47:23 +0100

    Update RandomCards.py

--- RandomCards.py
+++ RandomCards.py
@@ -1,5 +1,6 @@
 from itertools import  product
 from random import shuffle
+import itertools as it
 
 suits = ["C","D","H","S"]
 ranks = ["2","3","4","5","4","6","7","8","9","J","Q","K","A"]
@@ -8,10 +9,11 @@ cards = list(r + s for r,s in product(ranks,suits))
 shuffle(cards)
 
 def grouper(n, iterable, fillvalue=None):
-    from itertools import izip_longest
+    #from itertools import izip_longest
     "Collect data into fixed-length chunks or blocks"
     args = [iter(iterable)] * n
-    return izip_longest(fillvalue=fillvalue, *args)
+    return it.zip_longest(fillvalue=fillvalue, *args)
+    #return izip_longest(fillvalue=fillvalue, *args)
 
 
 hand = grouper(5, cards)
